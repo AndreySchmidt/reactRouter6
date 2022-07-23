b990095e30c986791da4e2d3f793fb1fbf63ad60
AndreySchmidt <ringnull@gmail.com>
2022-07-23 18:58:11 +0300

    SinglePage

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import { Routes, Route } from 'react-router-dom'
 import { HomePage } from './pages/HomePage'
 import { AboutPage } from './pages/AboutPage'
 import { BlogPage } from './pages/BlogPage'
+import { SinglePage } from './pages/SinglePage'
 import { NotFoundPage } from './pages/NotFoundPage'
+
 import { Layout } from './components/Layout'
 
 const App = (props) => {
@@ -14,6 +16,7 @@ const App = (props) => {
           <Route index element = { <HomePage /> } />
           <Route path = "about" element = { <AboutPage /> } />
           <Route path = "blog" element = { <BlogPage /> } />
+          <Route path = "blog/:id" element = { <SinglePage /> } />
           <Route path = "*" element = { <NotFoundPage /> } />
         </Route>
       </Routes>
---
+++ src/pages/SinglePage.jsx
@@ -0,0 +1,12 @@
+import { useParams } from 'react-router-dom'
+
+
+const SinglePage = ( props ) => {
+  return (
+    <div>
+      <h1>Single</h1>
+    </div>
+  )
+}
+
+export { SinglePage }
